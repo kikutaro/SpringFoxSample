39af4859d00ddebc16d1319644adacffb7bbd010
kikutaro <kikutaro526@yahoo.co.jp>
2019-03-04 20:49:06 +0900

    :wolf: First Commit.

---
+++ src/main/java/tech/kikutaro/springfox/DemoApplication.java
@@ -0,0 +1,22 @@
+package tech.kikutaro.springfox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping(value="/hello")
+	public String hello() {
+		return "hello";
+	}
+	
+}
